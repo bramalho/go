a2b662b3193f4d734052624ae2b9c480ea4e5b78
bruno.ramalho <bruno.ramalho@olx.com>
2018-08-03 16:29:11 +0100

    new deck

--- cards/deck.go
+++ cards/deck.go
@@ -4,7 +4,21 @@ import "fmt"
 
 type deck []string
 
-func (d deck) print()  {
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"♠", "♦", "♥", "♣"}
+	cardValues := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit + value)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
