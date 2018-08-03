121aa9e805a9b06ce9f0fe679ee49daa512c0a71
bruno.ramalho <bruno.ramalho@olx.com>
2018-08-03 18:11:23 +0100

    test deck positions

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length 52, but got %v", len(d))
 	}
+
+	if d[0] != "♠ A" {
+		t.Errorf("Expected first card of '♠ A', but got '%v'", d[0])
+	}
+
+	if d[len(d) - 1] != "♣ K" {
+		t.Errorf("Expected first card of '♣ K', but got '%v'", d[len(d) - 1])
+	}
 }
