c4af28454b33c503add24f2f825cb59c4d39b35d
bruno.ramalho <bruno.ramalho@olx.com>
2018-08-06 13:47:12 +0100

    embedding structs

--- structs/main.go
+++ structs/main.go
@@ -2,9 +2,16 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	address string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main() {
@@ -14,9 +21,19 @@ func main() {
 	//fmt.Println(bruno)
 	//fmt.Println(bruno.firstName + " " + bruno.lastName)
 
-	var bruno person
-	bruno.firstName = "Bruno"
-	bruno.lastName = "Ramalho"
+	//var bruno person
+	//bruno.firstName = "Bruno"
+	//bruno.lastName = "Ramalho"
+
+	bruno := person{
+		firstName: "Bruno",
+		lastName: "Ramalho",
+		contact: contactInfo{
+			email: "bruno@gmail.com",
+			address: "Lisbon",
+			zipCode: 123,
+		},
+	}
 
 	fmt.Printf("%+v", bruno)
 }
