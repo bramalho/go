7f0f433abc03100e526d625dfdee09af2dd390c0
bruno.ramalho <bruno.ramalho@olx.com>
2018-08-06 13:12:50 +0100

    structs

--- structs/main.go
+++ structs/main.go
@@ -9,8 +9,14 @@ type person struct {
 
 func main() {
 	//bruno := person{"Bruno", "Ramalho"}
-	bruno := person{firstName: "Bruno", lastName: "Ramalho"}
+	//bruno := person{firstName: "Bruno", lastName: "Ramalho"}
+	//
+	//fmt.Println(bruno)
+	//fmt.Println(bruno.firstName + " " + bruno.lastName)
 
-	fmt.Println(bruno)
-	fmt.Println(bruno.firstName + " " + bruno.lastName)
+	var bruno person
+	bruno.firstName = "Bruno"
+	bruno.lastName = "Ramalho"
+
+	fmt.Printf("%+v", bruno)
 }
