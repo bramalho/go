c15d13b98923faa41e7521f26ba028fe01dac61c
bruno.ramalho <bruno.ramalho@olx.com>
2018-08-17 14:41:25 +0100

    manipulate map

--- map/main.go
+++ map/main.go
@@ -3,11 +3,18 @@ package main
 import "fmt"
 
 func main()  {
+	//var colors map[string]string
+	//colors := make(map[string]string)
+
 	colors := map[string]string{
 		"red" : "#ff0000",
 		"black": "#000000",
 		"white": "#ffffff",
 	}
+
+	colors["black"] = "#000"
+
+	delete(colors, "black")
 
 	fmt.Println(colors)
 }
