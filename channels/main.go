312ecddf1127291f35d4b59e2d9253f2c24ff41b
bruno.ramalho <bruno.ramalho@olx.com>
2018-08-20 15:24:07 +0100

    check website

---
+++ channels/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string {
+		"http://google.com",
+		"http://github.com",
+		"http://golang.org",
+		"http://not_an_website_com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string)  {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, " is down!")
+		return
+	}
+
+	fmt.Println(link, " is up!")
+}
