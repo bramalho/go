1cbcc4a3da3f7adeb2e962a8e41784694562c3cc
bruno.ramalho <bruno.ramalho@olx.com>
2018-08-20 15:41:06 +0100

    channel

--- channels/main.go
+++ channels/main.go
@@ -13,18 +13,25 @@ func main() {
 		"http://not_an_website_com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<- c)
 }
 
-func checkLink(link string)  {
+func checkLink(link string, c chan string)  {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, " is down!")
+		c <- "Down!"
 		return
 	}
 
 	fmt.Println(link, " is up!")
+	c <- "Up!"
+	return
 }
