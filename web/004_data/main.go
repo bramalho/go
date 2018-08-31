fa9481c496d3c721a695efed2957180ace70e8fc
bruno.ramalho <bruno.ramalho@olx.com>
2018-08-31 15:58:13 +0100

    pass data

---
+++ web/004_data/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", "Bruno")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
