ca10d8436437b71a1340c140abb697188a982500
bruno.ramalho <bruno.ramalho@olx.com>
2018-08-31 15:48:20 +0100

    template gohtml

---
+++ web/002_template/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+func main()  {
+	tpl, err := template.ParseFiles("index.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer nf.Close()
+
+	err = tpl.Execute(nf, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
